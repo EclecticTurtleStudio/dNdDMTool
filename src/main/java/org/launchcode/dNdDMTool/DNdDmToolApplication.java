6c86feb5f786afb2ab927b5a7c8bdadb0e9223f0
Rebecca Roth <EclecticTurtleStudio@gmail.com>
2020-08-10 19:24:56 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/dNdDMTool/DNdDmToolApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.dNdDMTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DNdDmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DNdDmToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/dNdDMTool/DNdDmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.dNdDMTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DNdDmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
